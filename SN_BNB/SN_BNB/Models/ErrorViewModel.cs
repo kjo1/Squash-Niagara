a1303f28141694f00914aa46e3b41fc7b2e48a21
Kanghyun Jo <kjo1@ncstudents.niagaracollege.ca>
2019-02-15 12:14:09 -0500

    Created

---
+++ SN_BNB/SN_BNB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SN_BNB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
