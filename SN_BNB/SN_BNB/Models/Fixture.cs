32f54a2148b0f0e3ee8e541633069ebb2840aa16
ImSozo <45073307+ImSozo@users.noreply.github.com>
2019-02-26 16:17:29 -0500

    foundation fixes

--- SN_BNB/SN_BNB/Models/Fixture.cs
+++ SN_BNB/SN_BNB/Models/Fixture.cs
@@ -25,9 +25,13 @@ namespace SN_BNB.Models
         [Display(Name = "Away Team Score")]
         public int AwayScore { get; set; }
 
+        [Display(Name="Home Team")]
         public int HomeTeamID { get; set; }
+        [Display(Name = "Away Team")]
         public int AwayTeamID { get; set; }
+        [Display(Name = "Season")]
         public int SeasonID { get; set; }
+        [Display(Name = "Location")]
         public int LocationID { get; set; }
 
         public virtual Location Location { get; set; }
