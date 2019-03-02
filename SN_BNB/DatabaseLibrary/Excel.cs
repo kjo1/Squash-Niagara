dbc6155abdf0969c4583d57ce23f776d2825d1b2
Kevin McGrinder <kjmcgrinder@gmail.com>
2019-03-02 13:47:13 -0500

    excel fixture method PDL

--- SN_BNB/DatabaseLibrary/Excel.cs
+++ SN_BNB/DatabaseLibrary/Excel.cs
@@ -10,17 +10,28 @@ namespace DatabaseLibrary
 {
     class Excel
     {
-        /* The excel document will have three columns, in this order */
-        protected struct DataStruct
+        /* The excel document for users will have three columns, in this order */
+        protected struct UserStruct
         {
             string username;
             string password;
             string role;
         }
+
+        /* The excel document for Fixtures will have these columns, in this order */
+        protected struct FixtureStruct
+        {
+            string FixtureDateTime;
+            string Location;     //need to find the location id for sql statement
+            string HomeTeam;     //need to find team id for sql statement
+            string AwayTeam;     
+        }
+
+
         public void CreateUsers(string excelDocPath)
         {
             //create a struct to hold username, password, and role
-            DataStruct[] dataStructs = new DataStruct[2000];
+            UserStruct[] dataStructs = new UserStruct[2000];
 
             //receive excel file
             FileInfo file = new FileInfo(excelDocPath);
@@ -31,6 +42,23 @@ namespace DatabaseLibrary
             //update user table
             //send confirmation or error message
 
+        }
+
+        public void CreateFixtures(string excelDocPath)
+        {
+            //create a struct to hold fixture data
+            FixtureStruct[] dataStructs = new FixtureStruct[2000];
+
+            //receive excel file
+            FileInfo file = new FileInfo(excelDocPath);
+            ExcelPackage excelPackage = new ExcelPackage(file);
+            ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets[1];
+
+            //parse the file and update struct
+            //find location id, hometeam id, and awayteam id
+            //make a new season
+            //update fixture table
+            //send confirmation or error message
         }
     }
 }
