8645cc122286a475494a0bbc1315ede9d9958469
Kevin McGrinder <kjmcgrinder@gmail.com>
2019-03-01 23:45:15 -0500

    DatabaseLibrary CRUD PDL created

--- SN_BNB/DatabaseLibrary/DatabaseHandler.cs
+++ SN_BNB/DatabaseLibrary/DatabaseHandler.cs
@@ -8,10 +8,32 @@ namespace DatabaseLibrary
 {
     public class DatabaseHandler
     {
-        
-        public void Incoming()
-        {
 
+        public void Create()
+        {
+            //controller sends data to this function
+            //also sent is the table to be updated
+            //a create command is generated
+            //the command is sent to the database
+            //a confirmation or error message is returned
+        }
+
+        public void Update()
+        {
+            //controller sends data to this function
+            //also sent is the table to be updated
+            //an update command is generated
+            //the command is sent to the database
+            //a confirmation or error message is returned
+        }
+
+        public void Delete()
+        {
+            //controller sends data to this function
+            //also sent is the table to be updated
+            //a delete command is generated
+            //the command is sent to the database
+            //a confirmation or error message is returned
         }
     }
 }
