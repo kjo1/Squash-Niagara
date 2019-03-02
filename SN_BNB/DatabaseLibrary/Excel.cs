d52491ecc76fd6b8ab170f654a247600d30e42fc
Kevin McGrinder <kjmcgrinder@gmail.com>
2019-03-02 00:05:00 -0500

    bit more excel work

--- SN_BNB/DatabaseLibrary/Excel.cs
+++ SN_BNB/DatabaseLibrary/Excel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using OfficeOpenXml;
 
 namespace DatabaseLibrary
 {
@@ -14,11 +16,13 @@ namespace DatabaseLibrary
             string password;
             string role;
         }
-        public void CreateUsers()
+        public void CreateUsers(string excelDocPath)
         {
             //create a struct to hold username, password, and role
             DataStruct[] dataStructs = new DataStruct[2000];
             //receive excel file
+            FileInfo file = new FileInfo(excelDocPath);
+            ExcelPackage excelPackage = new ExcelPackage(file);
             //parse the file and update struct
             //update user table
             //send confirmation or error message
