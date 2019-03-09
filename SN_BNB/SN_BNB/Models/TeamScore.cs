d03803a939ce041d41486d7db2eb92b9c162e191
Kang <kjo0@outlook.com>
2019-03-09 00:20:45 -0500

    Matches, Players, Teams - little change(Archive)
    TeamScores - Comments deleted (cuz Kevin doesn't like it)

--- SN_BNB/SN_BNB/Models/TeamScore.cs
+++ SN_BNB/SN_BNB/Models/TeamScore.cs
@@ -9,10 +9,6 @@ namespace SN_BNB.Models
     public class TeamScore
     {
 
-        //public TeamScore()
-        //{
-        //    this.Matches = new HashSet<Match>();
-        //}
         public int ID { get; set; }
 
         [Display(Name = "Fixture Score")]
@@ -28,6 +24,5 @@ namespace SN_BNB.Models
         public int FixtureID { get; set; }
         public virtual Fixture Fixture { get; set; }
 
-        //public virtual ICollection<Match> Matches { get; set; }
     }
 }
