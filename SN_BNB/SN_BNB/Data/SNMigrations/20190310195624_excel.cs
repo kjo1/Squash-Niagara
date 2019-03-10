7ed9a0d5e6d6399cd2d9a3d17f60538227cbb3fb
ImSozo <45073307+ImSozo@users.noreply.github.com>
2019-03-10 15:59:29 -0400

    nav fix

---
+++ SN_BNB/SN_BNB/Data/SNMigrations/20190310195624_excel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SN_BNB.Data.SNMigrations
+{
+    public partial class excel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
