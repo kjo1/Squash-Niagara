a6f5837b6050ac08fceb48a1fe95f884c77c8484
Kevin McGrinder <kjmcgrinder@gmail.com>
2019-03-15 21:33:22 -0400

    hide phone and email based on if player wants info hidden
    also modified Player model to begin working on excel upload

--- SN_BNB/SN_BNB/Models/Player.cs
+++ SN_BNB/SN_BNB/Models/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -68,6 +69,8 @@ namespace SN_BNB.Models
 
         public virtual Team Team { get; set; }
 
+        [NotMappedAttribute]
+        public Byte[] ExcelFile { get; set; }
         public ICollection<AssignedMatchPlayer> AssignedMatchPlayers { get; set; }
 
     }
