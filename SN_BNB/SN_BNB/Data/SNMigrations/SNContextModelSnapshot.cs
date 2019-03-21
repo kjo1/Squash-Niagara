883f72bbd33a218ef0e6609d2e8b0dc158cefba4
ImSozo <45073307+ImSozo@users.noreply.github.com>
2019-03-21 15:14:28 -0400

    Style fixes

--- SN_BNB/SN_BNB/Data/SNMigrations/SNContextModelSnapshot.cs
+++ SN_BNB/SN_BNB/Data/SNMigrations/SNContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace SN_BNB.Data.SNMigrations
 #pragma warning disable 612, 618
             modelBuilder
                 .HasDefaultSchema("SN")
-                .HasAnnotation("ProductVersion", "2.1.1-rtm-30846")
+                .HasAnnotation("ProductVersion", "2.1.4-rtm-31024")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
