26897b375333ae09d2f07b60fc10c2c55b91917f
Yorguin <helpdesk@niagaracollege.ca>
2019-03-22 12:29:15 -0400

    lots of changes

---
+++ SN_BNB/SN_BNB/ViewModels/TeamScheduleVM.cs
@@ -0,0 +1,23 @@
+﻿using SN_BNB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SN_BNB.ViewModels
+{
+    public class TeamScheduleVM
+    {
+        public TeamScheduleVM(Fixture fixture)
+        {
+            HomeTeamName = fixture.HomeTeam.TeamName;
+            AwayTeamName = fixture.AwayTeam.TeamName;
+            Date = fixture.FixtureDateTime.ToShortDateString();
+        }
+
+        public string HomeTeamName;
+        public string AwayTeamName;
+        public string Date;
+
+    }
+}
