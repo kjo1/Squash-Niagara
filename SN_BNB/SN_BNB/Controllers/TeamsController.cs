8368eae0aa96049fc892bd45e82e4d2c40946147
kjmcgrinder <kjmcgrinder@gmail.com>
2019-04-01 12:22:35 -0400

    fix Team ICollection Issue
    #6

--- SN_BNB/SN_BNB/Controllers/TeamsController.cs
+++ SN_BNB/SN_BNB/Controllers/TeamsController.cs
@@ -180,6 +180,7 @@ namespace SN_BNB.Controllers
                 .ThenInclude(t => t.AwayTeam)
                 .Include(t => t.AwayFixtures)
                 .ThenInclude(t => t.HomeTeam)
+                .Include(t=>t.Players)
                 .FirstOrDefaultAsync(m => m.ID == id);
 
             if (team == null)
@@ -188,7 +189,6 @@ namespace SN_BNB.Controllers
             }
 
             ViewBag.Fixtures = team.AwayFixtures.Union(team.HomeFixtures).Where(f => DateTime.Now.AddDays(30).CompareTo(f.FixtureDateTime) > 0).OrderBy(f => f.FixtureDateTime).Select(t => new TeamScheduleVM(t));
-            ViewBag.Players = _context.Players.Where(t => t.TeamID == team.ID);     //due to trouble finding a list of players with Team.Players, this is used
 
             return View(team);
         }
